e094928680dd663250f91d668aa35bbdb6ceb6bc
Asif Forizy <asifforizy@gmail.com>
2026-08-30 10:10:05 +0600

    login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,7 @@
+import LoginForm from "../_components/LoginForm";
 
-
-const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
+export default function page() {
+  return <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <LoginForm></LoginForm>
+  </div>
 }
-
-export default LoginPage
